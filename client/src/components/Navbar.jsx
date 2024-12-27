d10edf64a1cd048b109277d38e51ffb626ce5186
gaurav <gaurav1452001@gmail.com>
2024-12-28 03:17:26 +0530

    basic clerk features added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import logo from '../assets/logo.png';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
-  return (
-    <div className='shadow px-7'>
-      <div className='flex justify-between items-center'>
-        <div className='w-40'>
-            <img src={logo} alt="logo" />
+
+    const { openSignIn } = useClerk()
+    const { user } = useUser()
+
+    return (
+        <div className='shadow px-7'>
+            <div className='flex justify-between items-center'>
+                <div className='w-40'>
+                    <img src={logo} alt="logo" />
+                </div>
+                {
+                    user ? <div>
+                        <UserButton/>
+                    </div>
+                        : <div>
+                            <button onClick={e => openSignIn()} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+                                Login
+                            </button>
+                        </div>
+                }
+
+
+            </div>
         </div>
-        <div>
-            <button>Login</button>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
