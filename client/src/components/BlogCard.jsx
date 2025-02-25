de341eb08a79561f1eff2b3cd7f874ea4aa3bb42
gaurav <gaurav1452001@gmail.com>
2025-02-25 19:08:52 +0530

    placeholder images inserted

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -5,12 +5,12 @@ import {blogData} from '../assets/assets'
 const BlogCard = ({ blog }) => { 
   return (
     <>
-      <div className='w-1/2 border-2 mx-auto '>
+      <div className='w-1/2 border-2 mx-auto'>
       </div>
-      <div className='hover:bg-[#9cb5f5] m-3 p-4 flex-col mx-auto sm:w-2/3 md:w-1/2'>
+      <div className='hover:bg-[#9cb5f5] m-3 p-4 flex-col mx-auto sm:w-2/3 md:w-1/2 cursor-pointer'>
         <div className='flex flex-row mx-2'>
-          <img src="https://dub.sh/Y0NxRWv" alt="" className='h-7 w-7 rounded-full object-cover object-center' />
-          <p className='mx-4'>{blog.title}</p>
+          <img src="https://placehold.co/600x400/000000/FFF" alt="" className='h-7 w-7 rounded-full object-cover object-center' />
+          <p className='mx-4 font-semibold'>{blog.title}</p>
         </div>
         <div className='m-3 flex flex-row justify-between'>
           <div className='flex flex-col gap-4'>
@@ -19,7 +19,7 @@ const BlogCard = ({ blog }) => {
             <p className='text-xs'>{blog.author}</p>
           </div>
           <div>
-            <img src={blog.image} alt="Stock Image" className='w-44  rounded-lg' />
+            <img src={blog.image} alt="Stock Image" className='w-60 object-cover rounded-lg' />
           </div>
         </div>
       </div>
