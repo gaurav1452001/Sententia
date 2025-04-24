804a95e3495cada9d71477e4750ea6f6566eee4e
gaurav <gaurav1452001@gmail.com>
2025-04-24 19:54:15 +0530

    increase visit added

--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { getPosts,getPost,createPost,deletePost,uploadAuth, getUserPost } from '../controllers/post.controller.js'
+import increaseVisit from '../middleware/increaseVisit.js';
 const router = express.Router()
 
 router.get('/upload-auth', uploadAuth);
 router.get('/', getPosts);
 router.get('/users/:userId', getUserPost);
-router.get('/:slug', getPost);
+router.get('/:slug', increaseVisit,getPost);
 router.post('/', createPost);
 router.delete('/:id', deletePost);
 
